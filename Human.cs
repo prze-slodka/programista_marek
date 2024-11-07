f45980616d04eae16b350b7496a621f7a90a8c28
Anie <72042709+prze-slodka@users.noreply.github.com>
2024-11-07 19:51:11 +0100

    Update Human.cs

--- Human.cs
+++ Human.cs
@@ -1,1 +1,17 @@
+namespace C3
+{
+    public abstract class Human : ITransportable
+    {
+        public double Weight { get; set; }
+        public double Volume { get; set; }
 
+        public Human(double weight, double volume)
+        {
+            Weight = weight;
+            Volume = volume;
+        }
+
+        public double GetWeight() => Weight;
+        public double GetVolume() => Volume;
+    }
+}
