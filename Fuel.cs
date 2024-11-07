f81e8b37bd7b4c2b042b3a15438528f40b846074
Anie <72042709+prze-slodka@users.noreply.github.com>
2024-11-07 19:08:02 +0100

    Update Fuel.cs

--- Fuel.cs
+++ Fuel.cs
@@ -1,1 +1,17 @@
+public abstract class Fuel
+{
+    public abstract string GetType();
+    public abstract double Density { get; }
+}
 
+public class FuelNuclear : Fuel
+{
+    public override string GetType() => "nuclear";
+    public override double Density => 10; // Example density value
+}
+
+public class FuelDiesel : Fuel
+{
+    public override string GetType() => "diesel";
+    public override double Density => 8; // Example density value
+}
