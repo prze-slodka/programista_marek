2c550ef35cd2a4a95b95fb7d41f29ed3cb153f6a
Anie <72042709+prze-slodka@users.noreply.github.com>
2024-11-07 19:51:36 +0100

    Update Crewman.cs

--- Crewman.cs
+++ Crewman.cs
@@ -1,1 +1,9 @@
-
+namespace C3
+{
+    public class Crewman : Human
+    {
+        public Crewman() : base(weight: 70, volume: 0.07) // Example values for weight and volume
+        {
+        }
+    }
+}
