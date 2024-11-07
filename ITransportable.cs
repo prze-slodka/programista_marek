07714304032a15b2ca880786563a103d82c08a80
Anie <72042709+prze-slodka@users.noreply.github.com>
2024-11-07 19:55:30 +0100

    Update ITransportable.cs

--- ITransportable.cs
+++ ITransportable.cs
@@ -4,15 +4,9 @@ using System.Text;
 
 namespace C3
 {
-    interface ITransportable
+    public interface ITransportable
     {
-        // anything we can transport is ITransportable
-        // examples: people, food, waste, fuel, equipment
-        // each transportable item has to know its volume in m^3 and weight in tonnes
-        // important: if you change volume, weight should update automatically and vice versa
-        // you can use any numbers you want - don't worry about being realistic
-		// see Equipment.cs for an example implementation
-        double Volume { get; set; } 
-        double Weight { get; set; }
+        double GetWeight();
+        double GetVolume();
     }
 }
