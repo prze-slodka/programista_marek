ef59212be7abde235ad2fab6be289e9f7b65efe2
Anie <72042709+prze-slodka@users.noreply.github.com>
2024-11-07 19:08:32 +0100

    Update FuelTank.cs

--- FuelTank.cs
+++ FuelTank.cs
@@ -1,1 +1,19 @@
+public class FuelTank : IVisitPort
+{
+    private Fuel _fuel;
+    public double MaxCapacity { get; }
+    
+    public FuelTank(double capacity, Fuel fuel)
+    {
+        MaxCapacity = capacity;
+        _fuel = fuel;
+    }
 
+    public double GetCapacity() => MaxCapacity;
+    public string GetFuelType() => _fuel.GetType();
+    
+    public void VisitPort()
+    {
+        // Implement logic to adjust volume and weight
+    }
+}
